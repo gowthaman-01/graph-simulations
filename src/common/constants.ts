90900c6a2b8dfa4b42aa6ad148b3fca67f6145f5
Gowthaman <gowthaman169@gmail.com>
2024-09-11 16:20:37 +0800

    Update tutorial page numbers and constants naming

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -18,13 +18,17 @@ export const SHORTEST_PATH_DELAY_MULTIPLIER = DEFAULT_DELAY;
 export const DEBOUNCE_DELAY: number = 100;
 
 // Tutorial properties.
-export const TOTAL_TUTORIAL_PAGES: number = 10;
+export const TOTAL_TUTORIAL_PAGES: number = 11;
 
 // Grid properities
 export const GRID_WIDTH: number = 580;
 export const DEFAULT_GRID_SIZE: number = 400;
 export const DEFAULT_HEURISTIC_MULTIPLIER: number = 5;
 export const MAX_HEURISTIC_MULTIPLIER: number = 100;
-export const LOCAL_STORAGE_KEY = 'pathium-global-variables';
+
+// Progress bar
 export const DEFAULT_PROGRESS_BAR_WIDTH = '10%';
 export const PROGRESS_BAR_INCREMENT = 5;
+
+// Local storage
+export const LOCAL_STORAGE_KEY = 'pathium-global-variables';
